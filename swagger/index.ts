84e9b2fa154f1da64a1db67ab9023fad0d69e81f
Özgür Adem IŞIKLI <i.ozguradem@gmail.com>
2023-09-16 21:51:48 +0200

    Fixed

---
+++ swagger/index.ts
@@ -0,0 +1,13 @@
+export default {
+  info: {
+    title: "Bookstore API",
+    description: "Description",
+  },
+  servers: [
+    {
+      url: "https://bookstore.axe-api.com",
+    },
+  ],
+  tags: [],
+  components: {},
+};
