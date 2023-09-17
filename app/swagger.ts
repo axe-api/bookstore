bd600594887095db59719d7582293fe211f272db
Özgür Adem IŞIKLI <i.ozguradem@gmail.com>
2023-09-17 11:47:58 +0200

    Fixed swagger documentations

--- app/swagger.ts
+++ app/swagger.ts
@@ -1,13 +1,31 @@
 export default {
   info: {
     title: "Bookstore API",
-    description: "Description",
+    description:
+      "This API project is an example API project built with Axe API Framework.",
+    version: "1.0.0",
+    contact: {
+      email: "i.ozguradem@gmail.com",
+    },
+    license: {
+      name: "MIT License",
+      url: "https://github.com/axe-api/bookstore/blob/master/LICENSE",
+    },
   },
   servers: [
     {
       url: "https://bookstore.axe-api.com",
     },
   ],
-  tags: [],
+  tags: [
+    {
+      name: "axe-api",
+      description: "The Next Generation Rest API Framework",
+      externalDocs: {
+        description: "Find out more",
+        url: "http://v1.axe-api.com",
+      },
+    },
+  ],
   components: {},
 };
