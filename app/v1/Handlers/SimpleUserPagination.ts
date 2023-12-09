c33e5175a7b71dfc1a280e51bb3e7bb87037f289
Özgür Adem IŞIKLI <i.ozguradem@gmail.com>
2023-12-09 22:12:41 +0100

    Fixed benchmarks

--- app/v1/Handlers/SimpleUserPagination.ts
+++ app/v1/Handlers/SimpleUserPagination.ts
@@ -1,12 +1,9 @@
 import { Request, Response } from "express";
 import { IoCService } from "axe-api";
 import { Knex } from "knex";
-import { attachPaginate } from "knex-paginate";
 
 export default async (req: Request, res: Response) => {
   const db = await IoCService.use<Knex>("Database");
-  const users = await db
-    .table("users")
-    .paginate({ perPage: 10, currentPage: 1 });
-  res.json(users);
+  const user = await db.table("users").where("id", 1);
+  res.json(user);
 };
---
+++ benchmark/custom-users.js
@@ -0,0 +1,23 @@
+import http from "k6/http";
+import { check, sleep } from "k6";
+
+export let options = {
+  stages: [
+    { duration: "30s", target: 50 },
+    { duration: "1m", target: 50 },
+    { duration: "30s", target: 25 },
+  ],
+  thresholds: {
+    http_req_duration: ["p(95)<500"],
+  },
+};
+
+export default function () {
+  // Make a GET request to your API endpoint
+  let response = http.get("https://bookstore.axe-api.com/express-users");
+
+  // Check if the request was successful (status code 200)
+  check(response, {
+    "status is 200": (r) => r.status === 200,
+  });
+}
