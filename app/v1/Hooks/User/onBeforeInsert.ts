77c04e1d2cda6c0af59a3b199291912cdd133a96
Özgür Adem IŞIKLI <i.ozguradem@gmail.com>
2023-12-19 23:32:01 +0100

    Fixed #7

--- app/v1/Hooks/User/onBeforeInsert.ts
+++ app/v1/Hooks/User/onBeforeInsert.ts
@@ -2,7 +2,21 @@ import bcrypt from "bcrypt";
 import { IContext } from "axe-api";
 import { UserStatus } from "../../Enum";
 
-export default async ({ formData }: IContext) => {
+export default async ({ formData, database, res }: IContext) => {
+  const { username, email } = formData;
+
+  const user = await database
+    .table("users")
+    .where("username", username)
+    .orWhere("email", email)
+    .first();
+
+  if (user) {
+    return res.status(400).json({
+      error: "The provided username or email is already in use.",
+    });
+  }
+
   formData.password = bcrypt.hashSync(formData.password, 10);
   formData.user_status = UserStatus.Standart;
 };
